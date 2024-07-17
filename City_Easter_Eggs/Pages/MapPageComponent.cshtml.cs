cc87c4fa584a32fe96ceecc3357352643ea12693
HHristova <hristiana_h@yahoo.com>
2024-07-17 14:46:12 +0300

    Moving Map page to it's own partial page

---
+++ City_Easter_Eggs/Pages/MapPageComponent.cshtml.cs
@@ -0,0 +1,22 @@
+﻿#region Using
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#endregion
+
+namespace City_Easter_Eggs.Pages
+{
+    public class MapPageComponent : PageModel
+    {
+        private readonly ILogger<AboutModel> _logger;
+
+        public MapPageComponent(ILogger<AboutModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
